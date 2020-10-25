1c4b12046888e6f2adc6424061108b09f55cae19
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-10-25 03:05:17 -0400

    removed response class methods

--- response.h
+++ response.h
@@ -11,17 +11,14 @@ struct Response
 {
 	// buffer for response
 	const static int bufferSize = 1024;
-	char buffer[bufferSize] = {0};
+	char buffer[bufferSize] = {0}; // buffer read() writes to
 	string status;				 // status line
 	bool success = true;		  // depends on status
 	map<string, string> headers; // header lines
-	string body;				 // body of request
+	string body;				 // body of response
 	int bytesRead = 0;				// bytes read from response
 	time_t time = 0;					// response time
 
 	// constructors
-	Response(){};		// default
-	Response(char *r); // with response str
-
-	bool setHeadersAndBody(char responseBuffer[]); // get data from response str
+	Response(){};
 };
